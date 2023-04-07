c7404402545e565d0cfdb8e158f092e4efd8b18c
Abubakar sani <ahmedmusa00078@gmail.com>
2023-04-07 07:55:48 +0100

    Created the Model for user

---
+++ server/model/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide unique Username"],
+        unique: [true, "Username Already Exist"]
+    },
+    password: {type: String, required: [true, "Please provide a password"], unique: false},
+    email: {type: String, required: [true, "Please provide an Email"], unique: true},
+    firstname: {type: String},
+    lastname: {type: String},
+    mobile: {type: Number},
+    address: {type: String},
+    profile: {type: String},
+
+});
+
+export default mongoose.model.Users || mongoose.model('User',UserSchema);
--- server/router/routes.js
+++ server/router/routes.js
@@ -1,23 +1,24 @@
 import {Router} from "express";
+import * as controller from '../controllers/appController.js'
 
 const router = Router();
 
 
 /*POST Method*/
-router.route('/register').post((req,res)=> res.json('register route')); // register user
+router.route('/register').post(controller.register); // register user
 router.route('/registerMail').post(); // send the mail
-router.route('/authenticate').post(); //authenticate user
-router.route('/login').post(); // login in app
+router.route('/authenticate').post((req, res) => res.end()); //authenticate user
+router.route('/login').post(controller.login); // login in app
 
 
 /*GET Method*/
-router.route('/user/:username').get(); // user with username
-router.route('/generateOTP').get(); // generate random OTP
-router.route('/verifyOTP').get(); // verify generated OTP
-router.route('/createResetSession').get(); // reset all the variables
+router.route('/user/:username').get(controller.getUser); // user with username
+router.route('/generateOTP').get(controller.generateOTP); // generate random OTP
+router.route('/verifyOTP').get(controller.verifyOTP); // verify generated OTP
+router.route('/createResetSession').get(controller.createResetSession); // reset all the variables
 
 /*PUT Method*/
-router.route('/updateuser').put(); // update the user profile
-router.route('/resetPassword').put(); // used to rest password
+router.route('/updateuser').put(controller.updateUser); // update the user profile
+router.route('/resetPassword').put(controller.resetPassword); // used to rest password
 
 export default router
